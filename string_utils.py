63dcbbc668f81774ddebbc67b27d36de7aae0aa1
am32626 <ayalamaman00@gmail.com>
2025-11-17 22:40:20 +0200

    Implement string splitting functions

--- string_utils.py
+++ string_utils.py
@@ -1,6 +1,23 @@
-def split_before_each_uppercases(formula):
-    pass # Replace the `pass` with your code
+def split_before_each_uppercase(e):
+    prefix = ""
+    uppercase = []
+    for char in e:
+        if char.isupper():
+            if prefix != "":
+                uppercase.append(prefix)
+            prefix = char
+        else:
+            prefix += char
+    if prefix != "":
+        uppercase.append(prefix)
+    return uppercase
 
 
-def split_at_first_digit(formula):
-    pass # Replace the `pass` with your code
+def split_at_first_digit(s): 
+    prefix = ""
+    for char in s:
+        if char.isdigit():
+            return prefix, char
+        prefix += char
+    return prefix, "1"
+    
