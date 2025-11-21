b34c5e15d25f40f324590b9dbf915a203fcb3d32
am32626 <ayalamaman00@gmail.com>
2025-11-21 10:46:49 +0200

    Refactor string splitting functions for clarity

--- string_utils.py
+++ string_utils.py
@@ -1,27 +1,18 @@
-
 def split_before_each_uppercase(formula):
-    split_formula = []
-    if len(formula) == 0:
-        return []
     start = 0
-    for end in range(1, len(formula)):
-        if formula[end].issupper():
-            split_formula.append(formula[start:end])
-            start = end
-    split_formula.append(formula[start:])
-    return split_formula
+    list_of_items = []
+    for char in range(1, len(formula)):
+        if formula[char].isupper():
+            list_of_items.append(formula[start:char])
+            start = char
+    list_of_items.append(formula[start:])
+    return list_of_items
 
 
-def split_at_first_digit(formula):
-    digit_location = 0
-    for i, char in enumerate(formula):
-        if char.isdigit():
-            digit_location = i
-            break
-    if digit_location == 0:
-            return formula, 1
-    else:
-        prefix = formula[:digit location]
-        number = int(formula[digit_locatio:])
-        return prefix, number
+
+def split_at_digit(formula):
+    for i in range(len(formula)):
+        if formula[i].isdigit():
+            return formula[:i], int(formula[i:])
+    return formula, 1
     
