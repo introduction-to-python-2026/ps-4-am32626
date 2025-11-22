67330cfe9ef79a2ff602eeadbc3042ae4383da55
am32626 <ayalamaman00@gmail.com>
2025-11-22 22:39:54 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -14,5 +14,5 @@ def split_at_first_digit(formula):
     for i in range(len(formula)):
         if formula[i].isdigit():
             return formula[:i], int(formula[i:])
-    return formula, "1"
+    return formula, 1
     
