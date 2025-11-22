412a4892176aaed5e086304fe2b7a2263106f2d4
am32626 <ayalamaman00@gmail.com>
2025-11-22 22:52:29 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -1,18 +1,27 @@
-def split_before_each_uppercase(formula):
-    start = 0
-    list_of_items = []
-    for char in range(1, len(formula)):
-        if formula[char].isupper():
-            list_of_items.append(formula[start:char])
-            start = char
-    list_of_items.append(formula[start:])
-    return list_of_items
+# string_utils.py
 
+def split_before_each_uppercases(s: str) -> list:
+    if not s:
+        return []
 
+    parts = []
+    current = ""
+    for ch in s:
+        if ch.isupper():
+            if current:
+                parts.append(current)
+            current = ch
+        else:
+            current += ch
+    if current:
+        parts.append(current)
+    return parts
 
 def split_at_first_digit(formula):
-    for i in range(len(formula)):
-        if formula[i].isdigit():
-            return formula[:i], int(formula[i:])
+    for i, ch in enumerate(formula):
+        if ch.isdigit():
+            j = i + 1
+            while j < len(formula) and formula[j].isdigit():
+                j += 1
+            return formula[:i], int(formula[i:j])
     return formula, 1
-    
